256f24f5e460843d1c311ccaa0543a731ce2c4c7
fujoshik <gerganakoleva03@schoolmath.eu>
2023-04-11 13:59:54 +0000

    Feature - Introduce Roles

---
+++ RushHour.Domain/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour.Domain.Enums
+{
+    public enum Role
+    {
+        ADMIN,
+        PROVIDER_ADMIN,
+        EMPLOYEE,
+        CLIENT
+    }
+}
