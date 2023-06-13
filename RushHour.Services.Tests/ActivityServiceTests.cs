dfe3db1868243912a0558e362fed23f99967120c
fujoshik <gerganakoleva03@schoolmath.eu>
2023-06-13 11:31:52 +0000

    Add Domain Service Tests

---
+++ RushHour.Services.Tests/ActivityServiceTests.cs
@@ -0,0 +1,203 @@
+﻿using Moq;
+using RushHour.Domain.Abstractions.Repositories;
+using RushHour.Domain.Abstractions.Services;
+using RushHour.Domain.DTOs.AccountDtos;
+using RushHour.Domain.DTOs;
+using RushHour.Domain.Enums;
+using RushHour.Services.Services;
+using RushHour.Domain.DTOs.ActivityDtos;
+using RushHour.Domain.DTOs.EmployeeDtos;
+using RushHour.Domain.DTOs.ActivityEmployeeDtos;
+using System;
+
+namespace RushHour.Services.Tests
+{
+    public class ActivityServiceTests
+    {
+        public IActivityService service;
+        public Mock<IActivityRepository> activityRepoMock;
+        public Mock<IAccountRepository> accountRepoMock;
+        public Mock<IEmployeeService> employeeServiceMock;
+        public Mock<IEmployeeRepository> employeeRepoMock;
+        public Mock<IActivityEmployeeRepository> activityEmployeeRepoMock;
+
+        public ActivityServiceTests() 
+        {
+            activityRepoMock = new Mock<IActivityRepository>();
+            accountRepoMock = new Mock<IAccountRepository>();
+            employeeServiceMock = new Mock<IEmployeeService>();
+            employeeRepoMock = new Mock<IEmployeeRepository>();
+            activityEmployeeRepoMock = new Mock<IActivityEmployeeRepository>();
+
+            activityRepoMock
+                .Setup(s => s.CreateAsync(It.IsAny<CreateActivityDto>()))
+                .ReturnsAsync(new GetActivityDto());
+            activityRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new GetActivityDto());            
+
+            accountRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new AccountDto());
+            accountRepoMock
+                .Setup(s => s.CreateAsync(It.IsAny<CreateAccountDto>(), It.IsAny<byte[]>()))
+                .ReturnsAsync(new AccountDto());
+            accountRepoMock
+                .Setup(s => s.CheckIfAnyMatchesIdAndRole(It.IsAny<Guid>(), It.IsAny<Role>()))
+                .ReturnsAsync(false);
+
+            activityEmployeeRepoMock
+                .Setup(s => s.CreateActivityWithManyEmployeesAsync(It.IsAny<Guid>(), It.IsAny<List<Guid>>()));
+            activityEmployeeRepoMock
+                .Setup(s => s.GetAllEmployeesOfActivityAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new List<ActivityEmployeeDto>());
+
+            service = new ActivityService(activityRepoMock.Object, accountRepoMock.Object,
+                null, activityEmployeeRepoMock.Object, null);
+        }
+
+        [Fact]
+        public async Task Create_ValidData_ExpectedNotNull()
+        {
+            // Arrange
+            var requesterId = Guid.NewGuid();
+            var providerId = Guid.NewGuid();
+            var employeeId = Guid.NewGuid();
+
+            var expected = new GetActivityDto
+            {
+                EmployeeIds = new List<Guid> { employeeId }
+            };
+
+            var employee = new GetEmployeeDto
+            {
+                Id = employeeId,
+                ProviderId = providerId,
+                Account = new GetAccountDto { Role = Role.ProviderAdmin }
+            };
+
+            var dto = new CreateActivityDto
+            {
+                ProviderId = providerId,
+                EmployeeIds = new List<Guid> { employeeId }
+            };
+
+            employeeRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(employee);
+
+            employeeServiceMock
+                .Setup(s => s.GetEmployeeByAccountAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(employee);
+
+            service = new ActivityService(activityRepoMock.Object, accountRepoMock.Object,
+                employeeServiceMock.Object, activityEmployeeRepoMock.Object, employeeRepoMock.Object);
+
+            //Act
+            var result = await service.CreateActivityAsync(requesterId, dto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equivalent(expected, result);
+        }
+
+        [Fact]
+        public async Task Update_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.UpdateActivityAsync(default(Guid), null, default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task GetById_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.GetActivityByIdAsync(default(Guid), default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task GetById_ValidData_ExpectedNotNull()
+        {
+            // Arrange
+            var requesterId = Guid.NewGuid();
+            var activityId = Guid.NewGuid();
+            var providerId = Guid.NewGuid();
+            var employeeId = Guid.NewGuid();
+
+            var employee = new GetEmployeeDto
+            {
+                Id = employeeId,
+                ProviderId = providerId,
+                Account = new GetAccountDto { Role = Role.ProviderAdmin }
+            };
+
+            var activity = new GetActivityDto
+            {
+                Id = activityId,
+                ProviderId = providerId
+            };
+            
+            activityRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(activity);
+
+            employeeServiceMock
+                .Setup(s => s.GetEmployeeByAccountAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(employee);
+
+            service = new ActivityService(activityRepoMock.Object, accountRepoMock.Object,
+                employeeServiceMock.Object, activityEmployeeRepoMock.Object, null);
+
+            // Act
+            var result = await service.GetActivityByIdAsync(requesterId, activityId);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equivalent(activity, result);
+        }
+
+        [Fact]
+        public async Task GetAll_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.GetPageAsync(1, 10, default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ValidParameters_ExpectedEmptyCollection()
+        {
+            // Arrange
+            var page = new PaginatedResult<GetActivityDto>(new List<GetActivityDto>(), 0);
+
+            activityRepoMock
+                .Setup(s => s.GetPageAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<Guid>()))
+                .ReturnsAsync(page);
+
+            // Act
+            var activities = await service.GetPageAsync(1, 10, Guid.NewGuid());
+
+            // Assert
+            Assert.NotNull(activities);
+            Assert.Equal(0, activities.Result.Count);
+        }
+
+        [Fact]
+        public async Task Delete_InvalidProviderId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.DeleteAsync(default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+    }
+}
---
+++ RushHour.Services.Tests/EmployeeServiceTests.cs
@@ -0,0 +1,173 @@
+﻿using Moq;
+using RushHour.Domain.Abstractions.Repositories;
+using RushHour.Domain.Abstractions.Services;
+using RushHour.Domain.DTOs;
+using RushHour.Domain.DTOs.AccountDtos;
+using RushHour.Domain.DTOs.EmployeeDtos;
+using RushHour.Domain.DTOs.ProviderDtos;
+using RushHour.Domain.Enums;
+using RushHour.Services.Services;
+
+namespace RushHour.Services.Tests
+{
+    public class EmployeeServiceTests
+    {
+        public IEmployeeService service;
+        public Mock<IEmployeeRepository> employeeRepoMock;
+        public Mock<IAccountRepository> accountRepoMock;
+        public Mock<IProviderRepository> providerRepoMock;
+        public Mock<IAccountService> accountServiceMock;
+
+        public EmployeeServiceTests() 
+        {
+            employeeRepoMock = new Mock<IEmployeeRepository>();
+            accountRepoMock = new Mock<IAccountRepository>();
+            providerRepoMock = new Mock<IProviderRepository>();
+            accountServiceMock = new Mock<IAccountService>();
+
+            var page = new PaginatedResult<GetEmployeeDto>(new List<GetEmployeeDto>(), 0);
+
+            employeeRepoMock
+                .Setup(s => s.CreateAsync(It.IsAny<GetAccountDto>(), It.IsAny<CreateEmployeeDto>()))
+                .ReturnsAsync(new GetEmployeeDto());
+            employeeRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new GetEmployeeDto());
+            employeeRepoMock
+                .Setup(s => s.GetPageAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .ReturnsAsync(page);
+
+            accountRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(new AccountDto());
+            accountRepoMock
+                .Setup(s => s.CreateAsync(It.IsAny<CreateAccountDto>(), It.IsAny<byte[]>()))
+                .ReturnsAsync(new AccountDto());
+
+            accountServiceMock.Setup(s => s.GenerateSalt());
+            accountServiceMock.Setup(s => s.HashPasword(It.IsAny<string>(), It.IsAny<byte[]>()));
+
+            service = new EmployeeService(employeeRepoMock.Object, accountRepoMock.Object,
+                providerRepoMock.Object, accountServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task Create_ValidData_ExpectedNotNull()
+        {
+            // Arrange
+            var dto = new CreateEmployeeDto
+            {
+                Account = new CreateAccountDto { Email = "test@test.com", Role = Role.Employee }
+            };
+
+            var provider = new GetProviderDto
+            {
+                BusinessDomain = "test"
+            };
+
+            var expected = new GetEmployeeDto();
+
+            providerRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(provider);           
+
+            service = new EmployeeService(employeeRepoMock.Object, accountRepoMock.Object,
+                providerRepoMock.Object, accountServiceMock.Object);
+
+            // Act
+            var result = await service.CreateEmployeeAsync(Guid.NewGuid(), dto);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equivalent(expected, result);
+        }
+
+        [Fact]
+        public async Task Create_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.CreateEmployeeAsync(default(Guid), null);
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task Update_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.UpdateEmployeeAsync(default(Guid), null, default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task GetById_InvalidRequesterId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.GetEmployeeByIdAsync(default(Guid), default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+
+        [Fact]
+        public async Task GetById_ValidData_ExpectedNotNull()
+        {
+            // Arrange
+            var requesterId = Guid.NewGuid();
+            var id = Guid.NewGuid();
+
+            var currentAccount = new AccountDto
+            {
+                Role = Role.Employee
+            };
+
+            var employeeToRead = new GetEmployeeDto
+            {
+                Id = id,
+                AccountId = requesterId
+            };
+
+            accountRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(currentAccount);
+
+            employeeRepoMock
+                .Setup(s => s.GetByIdAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(employeeToRead);
+
+            service = new EmployeeService(employeeRepoMock.Object, accountRepoMock.Object,
+                providerRepoMock.Object, accountServiceMock.Object);
+
+            // Act
+            var result = await service.GetEmployeeByIdAsync(requesterId, id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equivalent(employeeToRead, result);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ValidParameters_ExpectedEmptyCollection()
+        {
+            // Act
+            var employees = await service.GetPageAsync(1, 10, Guid.NewGuid());
+
+            // Assert
+            Assert.NotNull(employees);
+            Assert.Equal(0, employees.Result.Count);
+        }
+
+        [Fact]
+        public async Task Delete_InvalidProviderId_ExpectedException()
+        {
+            // Act
+            async Task a() => await service.DeleteAsync(default(Guid));
+
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(a);
+        }
+    }
+}
